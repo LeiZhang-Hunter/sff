4cc6565d66049dd488df4eddc4d7fc8c614f97db
zhanglei <zhanglei1@chelun.com>
2019-04-29 09:30:39 +0800

    文件加载兼容性解决

--- test/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zhanglei
- * Date: 19-3-4
- * Time: 下午1:38
- */
-
-
--- tool/load_file.h
+++ tool/load_file.h
@@ -13,8 +13,6 @@ void include_file(const char* filename,zval *return_value_ptr);
 
 void get_file_execute_dir(char *buf);
 
-//void get_file_data_to_hash_table(zval* config_collect,char *file_path);
-//void get_file_data_to_hash_table(HashTable *ht,char *file_path,char *folder_level[],int level);
 void get_file_data_to_hash_table(HashTable *ht, char *file_path);
 
 //获取文件名字
